699130f795ac43b6e11f8201a57311fae287a981
Pieter <pieter.bork@colorado.edu>
2018-03-10 11:38:20 -0700

    umbrella does not work!

--- call_examples.py
+++ call_examples.py
@@ -17,26 +17,52 @@ headers = {
 }
 
 def get_response(url, values=None):
+    print(url)
     if values:
         r = requests.post(url, data=json.dumps(values), headers=headers)
     else:
         r = requests.get(url, headers=headers)
     return r.text
 
+def investigate_domain(domain):
+    url = 'https://investigate.api.opendns.com/security/name/{}.json'.format(domain)
+    url = 'https://investigate.api.opendns.com/domains/{}/latest_tags'.format(domain)
+    url = 'https://investigate.api.umbrella.com/timeline/{}'.format(domain)
+    print(url)
+    resp = get_response(url)
+    print("investigate", resp)
+
+
+investigate_domain("www.ncicye.com")
+investigate_domain("www.dreamscreen.xyz")
+investigate_domain("www.internetbadguys.com")
+investigate_domain("www.ciilhk.com")
 
 resp = get_response('https://investigate.api.opendns.com/domains/categorization/amazon.com')
 print(resp)
 
 values = ["google.com", "yahoo.com"]
 resp = get_response('https://investigate.api.opendns.com/domains/categorization/', values)
-print(resp)
+print("domain categorization", resp)
 
 resp = get_response('https://investigate.api.opendns.com/domains/categories')
-print(resp)
+print("domain categories", resp)
 
 resp = get_response('https://investigate.api.opendns.com/domains/score/example.com')
-print(resp)
+print("domain score", resp)
 
 resp = get_response('https://investigate.api.opendns.com/domains/score/', values)
-print(resp)
+print("domain score", resp)
+
+resp = get_response('https://investigate.api.opendns.com/security/name/www.internetbadguys.com.json')
+print("security", resp)
+
+resp = get_response('https://investigate.api.opendns.com/domains/www.internetbadguys.com/latest_tags')
+print("domain investigate", resp)
+
+resp = get_response('https://investigate.api.opendns.com/dnsdb/name/a/homestarrunner.com.json')
+print("dnsdb", resp)
+
+resp = get_response('https://investigate.api.opendns.com/ips/173.255.113.232/latest_domains')
+print("latest domain", resp)
 
