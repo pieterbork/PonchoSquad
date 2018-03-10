a6108cf426ef790f622226eb47cba6bc386dbeed
Pieter <pieter.bork@colorado.edu>
2018-03-10 13:10:03 -0700

    import ready

--- call_examples.py
+++ call_examples.py
@@ -3,23 +3,6 @@ import requests
 import json
 import os, sys
 
-token = os.getenv('INVESTIGATE_TOKEN', False)
-token = "7be254dc-9e39-4d06-b729-3578827b20db"
-
-if not token:
-  print("ERROR: environment variable \'INVESTIGATE_TOKEN\' not set. Invoke script with \'INVESTIGATE_TOKEN=%YourToken% python scripts.py\'")
-  sys.exit(1)
-
-# domains/categorization
-
-spark_headers = {
-  'Authorization': 'Bearer ' + token
-}
-vt_headers = {
-    "Accept-Encoding": "gzip, deflate",
-    "User-Agent" : "gzip,  My Python requests library example client or username"
-}
-
 def get_response(url, values=None, headers=None, params=None):
     print(url)
     if values:
@@ -31,6 +14,10 @@ def get_response(url, values=None, headers=None, params=None):
     return r.text
 
 def investigate_domain(domain):
+    token = "7be254dc-9e39-4d06-b729-3578827b20db"
+    spark_headers = {
+      'Authorization': 'Bearer ' + token
+    }
     url = 'https://investigate.api.umbrella.com/timeline/{}'.format(domain)
     resp = get_response(url, headers=spark_headers)
     info = json.loads(resp)
@@ -43,20 +30,24 @@ def investigate_domain(domain):
         return None
 
 def get_vt_report(domain):
-        vtparams = {'apikey': '4837b1adf0d071cd02bd05953d59b3e20ff48bcccea185b37f2bc2a63fcc73d7', 'resource':domain}
-        resp = get_response('https://www.virustotal.com/vtapi/v2/url/report', params=vtparams, headers=vt_headers)
-        info = json.loads(resp)
-        scans = info['scans']
+    vt_headers = {
+        "Accept-Encoding": "gzip, deflate",
+        "User-Agent" : "gzip,  My Python requests library example client or username"
+    }
+    vtparams = {'apikey': '4837b1adf0d071cd02bd05953d59b3e20ff48bcccea185b37f2bc2a63fcc73d7', 'resource':domain}
+    resp = get_response('https://www.virustotal.com/vtapi/v2/url/report', params=vtparams, headers=vt_headers)
+    info = json.loads(resp)
+    scans = info['scans']
 
-        results = {"sources": set([]), "types": set([])}
-        for scan in scans:
-            detected = scans[scan]['detected']
-            result = scans[scan]['result']
-            if detected:
-                results["sources"].add(scan)
-                results["types"].add(result)
+    results = {"sources": set([]), "types": set([])}
+    for scan in scans:
+        detected = scans[scan]['detected']
+        result = scans[scan]['result']
+        if detected:
+            results["sources"].add(scan)
+            results["types"].add(result)
 #                print("{} Reported this as {}".format(scan, result))
-        return results
+    return results
 
 
 #investigate_domain('google.com')
@@ -68,39 +59,39 @@ def get_vt_report(domain):
 #resp = get_response('https://investigate.api.opendns.com/domains/categorization/amazon.com')
 #print(resp)
 
-values = ["google.com", "yahoo.com"]
-
-#nasty domains
-value = "fbl.com.sg"
-value = "wd4o.com"
-
-results = get_vt_report(value)
-print("MAH RESULTS", results)
-
-domain_is_bad = investigate_domain(value)
-if domain_is_bad:
-    #POST TO SPARK ROOM
-    print("Ohhh boi bad domain")
-quit()
-
-resp = get_response('https://investigate.api.opendns.com/domains/categories')
-print("domain categories", resp)
-
-resp = get_response('https://investigate.api.opendns.com/domains/score/example.com')
-print("domain score", resp)
-
-resp = get_response('https://investigate.api.opendns.com/domains/score/', values)
-print("domain score", resp)
-
-resp = get_response('https://investigate.api.opendns.com/security/name/www.internetbadguys.com.json')
-print("security", resp)
-
-resp = get_response('https://investigate.api.opendns.com/domains/www.internetbadguys.com/latest_tags')
-print("domain investigate", resp)
-
-resp = get_response('https://investigate.api.opendns.com/dnsdb/name/a/homestarrunner.com.json')
-print("dnsdb", resp)
-
-resp = get_response('https://investigate.api.opendns.com/ips/173.255.113.232/latest_domains')
-print("latest domain", resp)
-
+#values = ["google.com", "yahoo.com"]
+#
+##nasty domains
+#value = "fbl.com.sg"
+#value = "wd4o.com"
+#
+#results = get_vt_report(value)
+#print("MAH RESULTS", results)
+#
+#domain_is_bad = investigate_domain(value)
+#if domain_is_bad:
+#    #POST TO SPARK ROOM
+#    print("Ohhh boi bad domain")
+#quit()
+#
+#resp = get_response('https://investigate.api.opendns.com/domains/categories')
+#print("domain categories", resp)
+#
+#resp = get_response('https://investigate.api.opendns.com/domains/score/example.com')
+#print("domain score", resp)
+#
+#resp = get_response('https://investigate.api.opendns.com/domains/score/', values)
+#print("domain score", resp)
+#
+#resp = get_response('https://investigate.api.opendns.com/security/name/www.internetbadguys.com.json')
+#print("security", resp)
+#
+#resp = get_response('https://investigate.api.opendns.com/domains/www.internetbadguys.com/latest_tags')
+#print("domain investigate", resp)
+#
+#resp = get_response('https://investigate.api.opendns.com/dnsdb/name/a/homestarrunner.com.json')
+#print("dnsdb", resp)
+#
+#resp = get_response('https://investigate.api.opendns.com/ips/173.255.113.232/latest_domains')
+#print("latest domain", resp)
+#
